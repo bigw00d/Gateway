c2de5c6892d8d23502486a1e8b661e0b7879f8cc
bigw00d <karinkuro@gmail.com>
2016-02-13 11:06:56 +0900

    ■BLE機能の追加（ボタン操作による送受信まで確認）

--- app/src/main/java/ytech/prototype/osiris/gateway/MainActivity.java
+++ app/src/main/java/ytech/prototype/osiris/gateway/MainActivity.java
@@ -1,6 +1,16 @@
 package ytech.prototype.osiris.gateway;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -27,9 +37,13 @@ import java.io.FileOutputStream;
 import android.view.MotionEvent;
 import android.os.Environment;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
@@ -38,6 +52,10 @@ public class MainActivity extends AppCompatActivity implements HttpPostListener 
 
     // [Common]
     private static final String TAG = "MainActivity";
+    private TextView mMainText;
+    private TextView mBleStText;
+    private Handler mMainTextHandler;
+    private Handler mBleStHandler;
 
     // [GCM]
     private GoogleCloudMessaging gcm;
@@ -53,6 +71,21 @@ public class MainActivity extends AppCompatActivity implements HttpPostListener 
     private static final int quality = 75; //unit:percentage
     float resizeScaleWidth = 320;
     float resizeScaleHeight = 240;
+
+    // [BLE]
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothManager mBluetoothManager;
+    private BleStatus mStatus = BleStatus.DISCONNECTED;
+    static final int REQUEST_ENABLE_BT = 0;
+    private static final long SCAN_PERIOD = 10000; /** BLE 機器検索のタイムアウト(ミリ秒) */
+    private BluetoothGatt mBluetoothGatt;
+    private boolean mIsBluetoothEnable = false;
+    private static final String DEVICE_NAME = "BLE PushNotifAct";
+    private static final String DEVICE_SENSOR_SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"; /** 対象のサービスUUID(デバイスにより変える必要がある) */
+    private static final String DEVICE_RX_CHARACTERISTIC_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"; /** 対象のキャラクタリスティックUUID(デバイスにより変える必要がある) */
+    private static final String DEVICE_TX_CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"; /** 対象のキャラクタリスティックUUID(デバイスにより変える必要がある) */
+    private static final String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb"; /** キャラクタリスティック設定UUID(これは固定のようだ) */
+
 
     private SurfaceHolder.Callback callback = new SurfaceHolder.Callback() {
         @Override
@@ -97,15 +130,6 @@ public class MainActivity extends AppCompatActivity implements HttpPostListener 
             }
         });
 
-        Button button1 = (Button)findViewById(R.id.post_button);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         // [GCM]
         context = getApplicationContext();
         gcm = GoogleCloudMessaging.getInstance(this);
@@ -123,7 +147,244 @@ public class MainActivity extends AppCompatActivity implements HttpPostListener 
         SurfaceHolder holder = mySurfaceView.getHolder();
         holder.addCallback(callback);
 
+        // [BLE]
+        mIsBluetoothEnable = false;
+
+        Log.d(TAG, "get adapter");
+        BluetoothManager manager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = manager.getAdapter();
+
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Log.d(TAG, "no adapter");
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+        else {
+            Log.d(TAG, "found adapter");
+        }
+        mBleStText = (TextView)findViewById(R.id.blest_view);
+        mBleStText.setText("BLE:Init Status");
+
+        mBleStHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                mBleStText.setText(((BleStatus) msg.obj).name());
+            }
+        };
+
+        findViewById(R.id.connect_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connect();
+            }
+        });
+        findViewById(R.id.discon_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                disconnect();
+            }
+        });
+        findViewById(R.id.post_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send();
+            }
+        });
+
+
+        // [Common]
+        mMainText = (TextView)findViewById(R.id.main_text);
+        mMainText.setText("");
+
+        mMainTextHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                mMainText.setText((String)msg.obj);
+            }
+        };
+
     }
+
+    // [ble]
+    /** BLE機器をスキャンした際のコールバック */
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            Log.d(TAG, "device found: " + device.getName());
+            if (DEVICE_NAME.equals(device.getName())) {
+                Log.d(TAG, "match device name");
+                // 機器名が "SensorTag" であるものを探す
+
+                // 機器が見つかればすぐにスキャンを停止する
+                mBluetoothAdapter.stopLeScan(this);
+
+                // 機器への接続を試みる
+                mBluetoothGatt = device.connectGatt(getApplicationContext(), false, mBluetoothGattCallback);
+
+                // 接続の成否は mBluetoothGattCallback で受け取る
+            }
+        }
+    };
+
+    // 接続完了や機器からのデータ受信はここで処理する
+    private final BluetoothGattCallback mBluetoothGattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            // ①：connectGatt()の後でここに来る
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                // GATTへ接続成功
+                Log.d(TAG, "gatt connection success");
+                // サービスを検索する
+                gatt.discoverServices();
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                // GATT通信から切断された
+                setStatus(BleStatus.DISCONNECTED);
+                mBluetoothGatt = null;
+                mIsBluetoothEnable = false;
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            // ②：discoverServices()の後でここに来る
+            Log.d(TAG, "check deiscovered");
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Log.d(TAG, "gatt success");
+
+                /* test code
+                for (BluetoothGattService service : gatt.getServices()) {
+                	Log.d(TAG, "found uuid:" + service.getUuid().toString());
+                }
+                */
+
+                BluetoothGattService service = gatt.getService(UUID.fromString(DEVICE_SENSOR_SERVICE_UUID));
+                if (service == null) {
+                    // サービスが見つからなかった
+                    Log.d(TAG, "no service");
+                    setStatus(BleStatus.SERVICE_NOT_FOUND);
+                    mIsBluetoothEnable = false;
+                } else {
+                    // サービスを見つけた
+                    Log.d(TAG, "service found");
+                    setStatus(BleStatus.SERVICE_FOUND);
+
+                    BluetoothGattCharacteristic characteristic =
+                            service.getCharacteristic(UUID.fromString(DEVICE_RX_CHARACTERISTIC_UUID));
+
+                    if (characteristic == null) {
+                        // キャラクタリスティックが見つからなかった
+                        Log.d(TAG, "characteristic not found");
+                        setStatus(BleStatus.CHARACTERISTIC_NOT_FOUND);
+                        mIsBluetoothEnable = false;
+                    } else {
+                        // キャラクタリスティックを見つけた
+                        Log.d(TAG, "characteristic found");
+
+                        // Notification を要求する
+                        boolean registered = gatt.setCharacteristicNotification(characteristic, true);
+
+                        // Characteristic の Notification 有効化
+                        Log.d(TAG, "enable notification");
+                        BluetoothGattDescriptor descriptor = characteristic.getDescriptor(
+                                UUID.fromString(CLIENT_CHARACTERISTIC_CONFIG));
+                        Log.d(TAG, "set descriptor value");
+                        descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                        Log.d(TAG, "write discriptor");
+                        gatt.writeDescriptor(descriptor);
+
+                        if (registered) {
+                            // Characteristics通知設定完了
+                            Log.d(TAG, "enable notification complete");
+                            setStatus(BleStatus.NOTIFICATION_REGISTERED);
+                            mIsBluetoothEnable = true;
+                        } else {
+                            Log.d(TAG, "enable notification incomplete");
+                            setStatus(BleStatus.NOTIFICATION_REGISTER_FAILED);
+                            mIsBluetoothEnable = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt,
+                                         BluetoothGattCharacteristic characteristic,
+                                         int status) {
+            // ③データ到着時にここが実行される
+            Log.d(TAG, "onCharacteristicRead: " + status);
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                // READ成功
+            }
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt,
+                                            BluetoothGattCharacteristic characteristic) {
+            // Characteristicの値更新通知
+            Log.d(TAG, "onCharacteristicChanged");
+            if (DEVICE_RX_CHARACTERISTIC_UUID.equals(characteristic.getUuid().toString())) {
+                final byte[] data = characteristic.getValue();
+                String result = new String(data);
+                Log.d(TAG, "Received Data:" + result);
+                setRcvDataText(result);
+            }
+        }
+    };
+
+    private void setRcvDataText(String rcvString) {
+        Message msg = Message.obtain();
+        msg.obj = "Received Ble Data:" + rcvString;
+        mMainTextHandler.sendMessage(msg);
+    }
+
+
+    /** BLE機器を検索する */
+    private void connect() {
+        Log.d(TAG, "start searching");
+        mBleStHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // タイムアウト
+                Log.d(TAG, "timeout scan");
+                mBluetoothAdapter.stopLeScan(mLeScanCallback );
+            }
+        }, SCAN_PERIOD);
+
+        // スキャン開始
+        Log.d(TAG, "start scan");
+        mBluetoothAdapter.startLeScan(mLeScanCallback );
+    }
+
+    /** BLE 機器との接続を解除する */
+    private void disconnect() {
+
+        Log.d(TAG, "start disconnecting");
+        if (mBluetoothGatt != null) {
+            mBluetoothGatt.close();
+            mBluetoothGatt = null;
+            setStatus(BleStatus.CLOSED);
+        }
+        mIsBluetoothEnable = false;
+
+    }
+
+    private void send() {
+        if (mIsBluetoothEnable) {
+            Log.d(TAG, "send");
+            BluetoothGattService myService = mBluetoothGatt.getService(UUID.fromString(DEVICE_SENSOR_SERVICE_UUID));
+            BluetoothGattCharacteristic myChar = myService.getCharacteristic(UUID.fromString(DEVICE_TX_CHARACTERISTIC_UUID));
+            myChar.setValue("ABC");
+            mBluetoothGatt.writeCharacteristic(myChar);
+        }
+    }
+
+    private void setStatus(BleStatus status) {
+        mStatus = status;
+        mBleStHandler.sendMessage(status.message());
+    }
+
+
 
     // [GCM]
     private void registerInBackground() {
@@ -259,4 +520,24 @@ public class MainActivity extends AppCompatActivity implements HttpPostListener 
 
         return super.onOptionsItemSelected(item);
     }
+
+    private enum BleStatus {
+        DISCONNECTED,
+        SCANNING,
+        SCAN_FAILED,
+        DEVICE_FOUND,
+        SERVICE_NOT_FOUND,
+        SERVICE_FOUND,
+        CHARACTERISTIC_NOT_FOUND,
+        NOTIFICATION_REGISTERED,
+        NOTIFICATION_REGISTER_FAILED,
+        CLOSED
+        ;
+        public Message message() {
+            Message message = new Message();
+            message.obj = this;
+            return message;
+        }
+    }
+
 }
